ac8551ae11a668baaff856bf81d21e9239907b39
ekappelman <ekappelman@outlook.com>
2016-11-21 05:43:35 -0700

    Should be moving now.

--- DataFetch.R
+++ DataFetch.R
@@ -1,19 +1,42 @@
-get.data <- function(){
-syms <- read.table("NYSE.txt",header = 2, sep = "\t")
-base.URL <- "http://finance.yahoo.com/d/quotes.csv?s="
-sb <- grep("[A-Z]{4}|[A-Z]{3}",syms$Symbol,perl = F, value = T)
-symbol.string <- syms$Symbol[1]
-for(i in 2:length(syms$Symbol)){
-  symbol.string <- paste(symbol.string,syms$Symbol[i],sep="+")
+parse.data <- function(symbols,range){
+  base.URL <- "http://finance.google.com/finance/info?client=ig&q="
+  start = min(range)
+  end = max(range)
+  symbol.string <- paste0("NYSE:",symbols[start],",")
+  for(i in (start+1):end){
+    temp <- paste0("NYSE:",symbols[i],",")
+    symbol.string <- paste(symbol.string,temp,sep="")
+  }
+  URL <- paste(base.URL,symbol.string,sep="")
+  
+  data <- GET(URL)
+  now <- date()
+  bin <- content(data, "raw")
+  writeBin(bin, "data.txt")
+  
+  conn <- file("data.txt",open="r")
+  linn <-readLines(conn)
+  jstring <- "["
+  for (i in 3:length(linn)){
+    jstring <- paste0(jstring,linn[i])
+  }
+  close(conn)
+  file.remove("data.txt")
+  obj <- fromJSON(jstring)
+  
+  return(data.frame(Symbol = obj$t,Price = as.numeric(obj$l)))
 }
-URL <- paste(base.URL,symbol.string,"&f=nab2",sep="")
 
-data <- GET(URL)
-now <- date()
-bin <- content(data, "raw")
-writeBin(bin, "data.csv")
-price.data <- read.csv("data.csv",header = FALSE,col.names = c("Name","Ask","Real.Time.Ask"))
-file.remove("data.csv")
-price.data <- price.data[price.data$Name != "N/A",]
-return(list(price.data$Ask,now,price.data$Name,price.data$Real.Time.Ask))
+get.data <- function(){
+syms <- read.csv("NYSE.txt",header = 2, sep = "\t")
+sb <- grep("[A-Z]{4}|[A-Z]{3}",syms$Symbol,perl = F, value = T)
+result <- c()
+in.list <- list()
+list.seq <- seq(1,2901,100)
+  for(i in 1:(length(list.seq)-1)){
+    range <- list.seq[i]:list.seq[i+1]
+    result <- rbind(result,parse.data(sb,range))
+  }
+return(droplevels.data.frame(na.omit(result)))
 }
+
--- RTicker.R
+++ RTicker.R
@@ -1,42 +1,19 @@
 library(httr)
 library(dplyr)
+library(jsonlite)
 source('DataFetch.R')
 
-Sys.sleep(75*60)
+Sys.sleep(110*60)
 
-res <- get.data()
-sdata <- as.vector(res[[1]])
-names <- res[[3]]
-date <- res[[2]]
-stockdata.ask <- data.frame(names,sdata)
-colnames(stockdata.ask) <- c("Names",date)
-
-sdata <- as.vector(res[[4]])
-names <- res[[3]]
-date <- res[[2]]
-stockdata.real.time.ask <- data.frame(names,sdata)
-colnames(stockdata.real.time.ask) <- c("Names",date)
+frame.list <- list()
 
 ticker <- function(rest.time){
-  res <- get.data()
-  sdata <- as.vector(res[[1]])
-  names <- res[[3]]
-  date <- res[[2]]
-  cols <- colnames(stockdata.ask)
-  stockdata.ask <- cbind(stockdata.ask,sdata)
-  colnames(stockdata.ask) <- c(cols,date)
-  
-  sdata <- as.vector(res[[4]])
-  names <- res[[3]]
-  date <- res[[2]]
-  cols <- colnames(stockdata.real.time.ask)
-  stockdata.real.time.ask <- cbind(stockdata.real.time.ask,sdata)
-  colnames(stockdata.real.time.ask) <- c(cols,date)
-
+  df <- data.frame(get.data(),Date = date())
+  frame.list <- c(list(df),frame.list)
   Sys.sleep(rest.time)
 }
 
 for(i in 1:80){
  ticker(5*60)  
 }
-save(stockdata.ask,stockdata.real.time.ask,file = "RealTimeData.rda")
+save(frame.list,file = "RealTimeData.rda")
