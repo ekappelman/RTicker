7289fc34458f329c2d7adaa23f678dea3bf0c7d9
ekappelman <ekappelman@outlook.com>
2016-11-24 07:40:31 -0700

    Near completion

--- RTicker.R
+++ RTicker.R
@@ -3,17 +3,19 @@ library(dplyr)
 library(jsonlite)
 source('DataFetch.R')
 
-Sys.sleep(110*60)
+Sys.sleep(55*60)
 
 frame.list <- list()
 
 ticker <- function(rest.time){
+  ptm <- proc.time()
   df <- data.frame(get.data(),Date = date())
-  frame.list <- c(list(df),frame.list)
-  Sys.sleep(rest.time)
+  timer.time <- proc.time() - ptm
+  Sys.sleep(as.numeric(rest.time - timer.time[3]))
+  return(list(df))
 }
 
 for(i in 1:80){
- ticker(5*60)  
+  frame.list <- c(suppressWarnings(ticker(5 * 60)),frame.list)
 }
 save(frame.list,file = "RealTimeData.rda")
--- StockAnalysis.R
+++ StockAnalysis.R
@@ -1,11 +1,16 @@
 library(quantmod)
+
 syms <- read.table("NYSE.txt",header = 2, sep = "\t")
 smb <- grep("[A-Z]{4}",syms$Symbol,perl = F, value = T)
 getSymbols(smb)
 mat <- c()
 stocks <- c()
+stockList <- list()
+names <- c()
 for(i in 1:length(smb)){
   temp <- get(smb[i])
+  names <- c(names,smb[i])
+  stockList[[i]] <- as.numeric(getPrice(temp))
   len <- length(attributes(temp)$index)
   if(len < 1001) next
   stocks <- c(stocks,smb[i])
@@ -13,54 +18,79 @@ for(i in 1:length(smb)){
   vex <- as.numeric(getPrice(temp2))
   mat <- rbind(mat,vex)
 }
-quant
+names(stockList) <- names
+
 stock.mat <- as.matrix(mat)
 row.names(stock.mat) <- stocks
 colnames(stock.mat) <- as.character(index(temp2))
-save(stock.mat,file = "StockData.rda")
+save(stock.mat,stockList,file = "StockData.rda")
 rm(list = ls())
+
 library(mclust)
 library(vars)
-
-cl <- Mclust(stock.mat,G = 1:20)
-stock.mat <- cbind(stock.mat,cl$classification)
-
-
-
-
 library(ggplot2)
 load("StockData.rda")
-cost_df <- data.frame()
-for(i in 1:100){
-  kmeans<- kmeans(x=stock.mat, centers=i, iter.max=100)
-  cost_df<- rbind(cost_df, cbind(i, kmeans$tot.withinss))
+
+ts <- ts(t(stock.mat))
+colnames(ts)
+ptm <- proc.time()
+model <- VAR(ts,p = 10)
+timer.time <- proc.time() - ptm
+
+cl <- Mclust(stock.mat,G = 1:9)
+stock.mat <- cbind(stock.mat,cl$classification)
+table(cl$classification)
+
+
+cluster <- stock.mat[stock.mat[,1002] == 6,1:1001]
+ts <- ts(t(cluster))
+fit <- VAR(ts[1:(1001-10),],p = 10)
+preds <- predict(fit, n.ahead = 10)
+forecast <- preds$fcst$TEVA
+plot.ts(ts[950:1001,8],ylim=c(36,54))
+lines(y = forecast[,1], x = (50 - 9):50,col = "blue")
+lines(y = forecast[,2], x = (50 - 9):50,col = "red",lty=2)
+lines(y = forecast[,3], x = (50 - 9):50,col = "red",lty=2)
+
+for(i in 1:8){
+  assign(paste0("cluster.",i),stock.mat[stock.mat[,1002] == i,1:1001])
+  assign(paste0("ts.",i),ts(t(get(paste0("cluster.",i)))))
+  temp <- get(paste0("ts.",i))
+  assign(paste0("fit.",i),VAR(temp, p = 10))
+  assign(paste0("preds.",i),predict(get(paste0("fit.",i)),n.ahead = 10))
 }
-names(cost_df) <- c("cluster", "cost")
-ggplot(data=cost_df, aes(x=cluster, y=cost)) +
-  geom_line(colour = "darkgreen") +
-  ggtitle("Reduction In Cost For Values of 'k'n") +
-  xlab("nClusters") +
-  ylab("Within-Cluster Sum of Squares n")
 
-kmeans<- kmeans(x=stock.mat, centers=50, iter.max=100)
-stock.mat <- cbind(stock.mat,kmeans$cluster)
-table(kmeans$cluster)
-cluster.eleven <- stock.mat[stock.mat[,1002] == 11,1:1001]
-ts.eleven <- ts(t(cluster.eleven))
-fit <- VAR(ts.eleven[1:(1001 - 56),], p = 56)
-preds <- predict(fit,n.ahead = 56)
-forecast <- preds$fcst$FSCE
-plot.ts(ts.eleven[900:1001,1],ylim = c(20,30))
-lines(y = forecast[,1], x = (100 - 55):100,col = "blue")
-lines(y = forecast[,2], x = (100 - 55):100,col = "red",lty=2)
-lines(y = forecast[,3], x = (100 - 55):100,col = "red",lty=2)
+stock.mat <- cbind(stock.mat,0)
+for(j in 1:8){
+  pred.vec <- c()
+  temp <- get(paste0("preds.",j))
+  for(i in temp$fcst){
+    cast <- temp$fcst[1]
+    cast <- cast[[1]]
+    cast <- cast[10,]
+    pred.vec <- c(pred.vec,cast[1])
+  }
+  stock.mat[stock.mat[,1002] == j,1003] <- pred.vec
+}
 
-fit <- VAR(ts.eleven[1:(1001 - 14),], p = 14)
-preds <- predict(fit,n.ahead = 14)
-forecast <- preds$fcst$FSCE
-plot.ts(ts.eleven[950:1001,1],ylim = c(24,27))
-lines(y = forecast[,1], x = (50 - 13):50,col = "blue")
-lines(y = forecast[,2], x = (50 - 13):50,col = "red",lty=2)
-lines(y = forecast[,3], x = (50 - 13):50,col = "red",lty=2)
+stock.mat <- stock.mat[stock.mat[,1002] != 9,]
+stock.mat[,1004] <- (stock.mat[,1003] - stock.mat[,1001]) / stock.mat[,1001] * 100
+stock.mat <- stock.mat[order(-stock.mat[,1004]),]
+stock.mat[1:10,1004]
 
-
+library(forecast)
+forecasts <- c()
+names <- c()
+for(i in 1:length(stockList)){
+  mod <- auto.arima(stockList[[i]])
+  cast <- forecast(mod)
+  cast <- cast$mean[10]
+  temp <- c(as.numeric(stockList[[i]][length(stockList[[i]])]), as.numeric(cast))
+  forecasts <- rbind(forecasts,temp)
+  names <- c(names,names(stockList[i]))
+}
+forecasts <- matrix(ncol = 2,forecasts)
+forecasts <- cbind(forecasts,(forecasts[,2] - forecasts[,1])/forecasts[,1] * 100)
+colnames(forecasts) <- c("Price","Forecast","% Change")
+row.names(forecasts) <- names
+forecasts <- forecasts[order(-forecasts[,3]),]
